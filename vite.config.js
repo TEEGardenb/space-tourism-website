9d900bb8f07808a4db24c0a99c19e6e681465a4d
MateoParra <mateoparra1171@gmail.com>
2024-02-14 01:11:20 -0500

    act-ruta de fondos

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://TEEGardenb.github.io/space-tourism-website",
+  base: "/space-tourism-website/",
   server: {
     host: "0.0.0.0",
   },
