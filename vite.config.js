2294ff51a9e3853130c35fa3743f4bbefe35b287
MateoParra <mateoparra1171@gmail.com>
2024-02-13 19:47:53 -0500

    preparacion para desplegar

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://TEEGardenb.github.io/space-tourism-website",
   server: {
     host: "0.0.0.0",
   },
